da6c0394ea22303f7d968a2837140d87ad41b6f7
Fran Dios <frankdiox@gmail.com>
2017-03-22 13:23:27 +0900

    Forms page.

--- src/pages/Forms.vue
+++ src/pages/Forms.vue
@@ -1,9 +1,144 @@
 <template>
   <v-ons-page>
+    <v-ons-list>
+      <v-ons-list-header>Text input</v-ons-list-header>
+      <v-ons-list-item>
+        <div class="left">
+          <v-ons-icon icon="md-face" class="list-item__icon"></v-ons-icon>
+        </div>
+        <label class="center">
+          <v-ons-input float maxlength="20"
+            placeholder="Input your name"
+            v-ons-model="name"
+          >
+          </v-ons-input>
+        </label>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="right">
+          Hello {{ name || 'anonymous' }}!<v-ons-icon icon="fa-hand-spock-o" class="right-icon"></v-ons-icon>
+        </div>
+      </v-ons-list-item>
+
+      <v-ons-list-header>Switches</v-ons-list-header>
+      <ons-list-item>
+        <label class="center" for="switch1">
+          Switch ({{ switchOn ? 'on' : 'off' }})
+        </label>
+        <div class="right">
+          <v-ons-switch input-id="switch1" v-ons-model="switchOn">
+          </v-ons-switch>
+        </div>
+      </ons-list-item>
+      <ons-list-item>
+        <label class="center" for="switch2">
+          {{ switchOn ? 'Enabled switch' : 'Disabled switch' }}
+        </label>
+        <div class="right">
+          <v-ons-switch input-id="switch2" :disabled="!switchOn">
+          </v-ons-switch>
+        </div>
+      </ons-list-item>
+
+      <v-ons-list-header>Select</v-ons-list-header>
+      <ons-list-item>
+        <div class="center">
+          <v-ons-select style="width: 200px" v-ons-model="selectedItem">
+            <option v-for="item in items" :value="item.value">
+              {{ item.text }}
+            </option>
+          </v-ons-select>
+
+        </div>
+        <div class="right">
+          {{ selectedItem }} is awesome!
+        </div>
+      </ons-list-item>
+
+      <v-ons-list-header>Radio buttons</v-ons-list-header>
+      <v-ons-list-item v-for="(vegetable, $index) in vegetables" :key="vegetable"
+        tappable
+        :modifier="($index === vegetables.length - 1) ? 'longdivider' : ''"
+      >
+        <label class="left">
+          <v-ons-input type="radio"
+            :input-id="'radio-' + $index"
+            :value="vegetable"
+            v-ons-model=" selectedVegetable"
+          >
+          </v-ons-input>
+        </label>
+        <label :for="'radio-' + $index" class="center">
+          {{ vegetable }}
+        </label>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          I love {{ selectedVegetable }}!
+        </div>
+      </v-ons-list-item>
+
+      <v-ons-list-header>Checkboxes - {{checkedColors}}</v-ons-list-header>
+      <v-ons-list-item v-for="(color, $index) in colors" :key="color" tappable>
+        <label class="left">
+          <v-ons-input type="checkbox"
+            :input-id="'checkbox-' + $index"
+            :value="color"
+            v-ons-model="checkedColors"
+          >
+          </v-ons-input>
+        </label>
+        <label class="center" :for="'checkbox-' + $index">
+          {{ color }}
+        </label>
+      </v-ons-list-item>
+
+      <v-ons-list-header>Range slider</v-ons-list-header>
+      <v-ons-list-item>
+        Adjust the volume:
+        <v-ons-row>
+          <v-ons-col width="40px" style="text-align: center; line-height: 31px;">
+            <v-ons-icon icon="md-volume-down"></v-ons-icon>
+          </v-ons-col>
+          <v-ons-col>
+
+            <v-ons-range v-ons-model.number="volume" style="width: 100%;"></v-ons-range>
+
+          </v-ons-col>
+          <v-ons-col width="40px" style="text-align: center; line-height: 31px;">
+            <v-ons-icon icon="md-volume-up"></v-ons-icon>
+          </v-ons-col>
+        </v-ons-row>
+        Volume: {{ volume }} <span v-show="volume > 80">&nbsp;(careful, that's loud)</span>
+      </v-ons-list-item>
+    </v-ons-list>
   </v-ons-page>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      name: '',
+      switchOn: true,
+      items: [
+        { text: 'Vue', value: 'Vue' },
+        { text: 'React', value: 'React' },
+        { text: 'Angular', value: 'Angular' }
+      ],
+      selectedItem: 'Vue',
+      vegetables: ['Apples', 'Bananas', 'Oranges'],
+      selectedVegetable: 'Bananas',
+      colors: ['Red', 'Green', 'Blue'],
+      checkedColors: ['Green', 'Blue'],
+      volume: 25
+    };
+  }
+};
 </script>
+
+<style scoped>
+.right-icon {
+  margin-left: 10px;
+}
+</style>
