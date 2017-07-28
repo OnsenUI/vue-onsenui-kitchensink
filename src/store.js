2d75e2a01fbf7823da55f73dc50fed1b2bcdbfde
Fran Dios <frankdiox@gmail.com>
2017-07-28 13:19:10 +0900

    Use === op.

--- src/store.js
+++ src/store.js
@@ -37,7 +37,7 @@ export default {
       },
       mutations: {
         toggle(state, shouldOpen) {
-          if (typeof shouldOpen == 'boolean') {
+          if (typeof shouldOpen === 'boolean') {
             state.open = shouldOpen;
           } else {
             state.open = !state.open;
