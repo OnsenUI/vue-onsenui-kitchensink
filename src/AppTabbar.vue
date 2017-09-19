d1094cdcc51951a99a45dbd71da7ed8265a852de
Fran Dios <frankdiox@gmail.com>
2017-09-19 19:32:48 +0900

    Make tabbar theme dynamic.

--- src/AppTabbar.vue
+++ src/AppTabbar.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-ons-page>
-    <custom-toolbar>
+  <v-ons-page :style="swipePosition">
+    <custom-toolbar :style="swipeTheme">
       {{ title }}
       <v-ons-toolbar-button slot="right" @click="$store.commit('splitter/toggle')">
         <v-ons-icon icon="ion-navicon, material:md-menu"></v-ons-icon>
       </v-ons-toolbar-button>
     </custom-toolbar>
 
-    <v-ons-tabbar
-      swipeable
-      position="auto"
-      modifier="autogrow"
+    <v-ons-tabbar position="auto" modifier="autogrow"
+      :swipeable="md()"
+      :on-swipe="md() ? onSwipe : null"
+      :animation="!md() && 'none'"
+      :tabbar-style="swipeTheme"
       :tabs="tabs"
       :index.sync="index"
     ></v-ons-tabbar>
@@ -23,39 +24,65 @@ import Home from './pages/Home.vue';
 import Forms from './pages/Forms.vue';
 import Animations from './pages/Animations.vue';
 
+// Just a linear interpolation formula
+const lerp = (x0, x1, t) => parseInt((1 - t) * x0 + t * x1, 10);
+// RGB colors
+const red = [231, 55, 47];
+const green = [0, 150, 136];
+const purple = [103, 58, 183];
+
 export default {
   data () {
     return {
+      colors: red,
+      animationOptions: {},
+      topPosition: 0,
       tabs: [
         {
           title: 'Camera',
           icon: 'md-camera',
           page: Camera,
-          style: 'max-width: 60px'
+          theme: red,
+          style: { maxWidth: '50px' },
+          top: -105 // Toolbar + Tabbar heights
         },
         {
           label: 'Home',
           icon: this.md() ? null : 'ion-home',
-          page: Home
+          page: Home,
+          theme: red
         },
         {
           label: 'Forms',
           icon: this.md() ? null : 'ion-edit',
-          page: Forms
+          page: Forms,
+          theme: green
         },
         {
           label: 'Anim',
           icon: this.md() ? null : 'ion-film-marker',
-          page: Animations
+          page: Animations,
+          theme: purple
         }
       ]
     };
   },
+
   methods: {
     md() {
       return this.$ons.platform.isAndroid();
-    }
+    },
+    onSwipe(index, animationOptions) {
+      // Apply the same transition as ons-tabbar
+      this.animationOptions = animationOptions;
+
+      // Interpolate colors and top position
+      const a = Math.floor(index), b = Math.ceil(index), ratio = index % 1;
+      this.colors = this.colors.map((c, i) => lerp(this.tabs[a].theme[i], this.tabs[b].theme[i], ratio));
+      this.topPosition = lerp(this.tabs[a].top || 0, this.tabs[b].top || 0, ratio);
+    },
   },
+
   computed: {
     index: {
       get() {
@@ -67,6 +94,18 @@ export default {
     },
     title() {
       return this.md() ? 'Onsen UI' : this.tabs[this.index].title || this.tabs[this.index].label;
+    },
+    swipeTheme() {
+      return this.md() && {
+        backgroundColor: `rgb(${this.colors.join(',')})`,
+        transition: `all ${this.animationOptions.duration || 0}s ${this.animationOptions.timing || ''}`
+      }
+    },
+    swipePosition() {
+      return this.md() && {
+        top: this.topPosition + 'px',
+        transition: `all ${this.animationOptions.duration || 0}s ${this.animationOptions.timing || ''}`
+      }
     }
   }
 };
