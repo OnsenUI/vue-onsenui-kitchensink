c6c9df59de96f2bd75ac84516cb609645b4bf215
Fran Dios <frankdiox@gmail.com>
2017-09-22 10:58:35 +0900

    Changed PullHook style for Material Design.

--- src/pages/PullHook.vue
+++ src/pages/PullHook.vue
@@ -3,18 +3,31 @@
     <custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
 
     <v-ons-pull-hook
-      threshold-height="120px"
       :action="onAction"
+      :fixed-content="md"
+      :height="md ? 84 : 64"
+      :on-pull="md && onPull || null"
       @changestate="state = $event.state"
     >
 
-      <v-ons-icon
+      <!-- Show this on iOS -->
+      <v-ons-icon v-if="!md"
         size="22px"
-        class="pull-hook-content"
+        class="pull-hook-spinner"
         :icon="state === 'action' ? 'fa-spinner' : 'fa-arrow-down'"
         :rotate="state === 'preaction' && 180"
         :spin="state === 'action'"
       ></v-ons-icon>
+
+      <!-- Show this on Material Design -->
+      <div v-else class="pull-hook-progress">
+        <v-ons-progress-circular
+          :value="ratio * 100"
+          :secondary-value="100"
+          :indeterminate="state === 'action'"
+          :style="{ transform: `rotate(${ratio}turn)` }"
+        ></v-ons-progress-circular>
+      </div>
 
     </v-ons-pull-hook>
 
@@ -36,11 +49,15 @@ export default {
   data() {
     return {
       state: 'initial',
-      kittens: this.getRandomData()
+      kittens: this.getRandomData(),
+      ratio: 0
     };
   },
 
   methods: {
+    onPull(ratio) {
+      this.ratio = ratio;
+    },
     onAction(done) {
       setTimeout(() => {
         this.kittens = [...this.kittens, this.getRandomKitten()];
@@ -49,7 +66,6 @@ export default {
     },
     getRandomName() {
       const names = ['Oscar', 'Max', 'Tiger', 'Sam', 'Misty', 'Simba', 'Coco', 'Chloe', 'Lucy', 'Missy'];
-
       return names[Math.floor(Math.random() * names.length)];
     },
     getRandomUrl() {
@@ -66,20 +82,46 @@ export default {
     },
     getRandomData() {
       const data = [];
-
       for (let i = 0; i < 8; i++) {
         data.push(this.getRandomKitten());
       }
-
       return data;
     }
   }
 };
 </script>
 
-<style scoped>
-.pull-hook-content {
+<style>
+.pull-hook-spinner {
   color: #666;
   transition: transform .25s ease-in-out;
+}
+
+.pull-hook-progress {
+  background-color: white;
+  width: 32px;
+  height: 32px;
+  margin: 30px auto 0;
+  border-radius: 100%;
+  position: relative;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  display: inline-block;
+  line-height: 0px;
+}
+
+.pull-hook-progress .progress-circular {
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 4px;
+  left: 4px;
+}
+
+.pull-hook-progress .progress-circular__primary {
+  transition: stroke-dashoffset 0s;
+}
+
+.pull-hook-progress .progress-circular__secondary {
+  stroke: rgba(84, 139, 167, 0.2);
 }
 </style>
