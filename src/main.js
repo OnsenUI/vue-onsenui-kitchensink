3d1124f09b3b5d65c75bc2131cc322bcca2bdd23
Fran Dios <frankdiox@gmail.com>
2017-11-10 18:17:31 +0900

    Update main.js to show ES Modules.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import VueOnsen from 'vue-onsenui';
-import CustomToolbar from './partials/CustomToolbar.vue';
-import storeLike from './store.js';
-import AppNavigator from './AppNavigator.vue';
-
 import 'onsenui/css/onsenui.css'; // Onsen UI basic CSS
 // import 'onsenui/css/onsen-css-components.css'; // Default Onsen UI CSS components
 import './onsen-css-components.css'; // Onsen UI CSS components source for custom themes (requires cssnext)
 
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueOnsen from 'vue-onsenui'; // For UMD
+// import VueOnsen from 'vue-onsenui/esm'; // For ESM
+// import * as OnsenComponents from './onsen-components'; // For ESM
+import storeLike from './store.js';
+import CustomToolbar from './partials/CustomToolbar.vue';
+import AppNavigator from './AppNavigator.vue';
+
 Vue.use(Vuex);
 Vue.use(VueOnsen);
+
+// Register components globally
+// Object.values(OnsenComponents).forEach(component => Vue.component(component.name, component)); // For ESM
 Vue.component('custom-toolbar', CustomToolbar); // Common toolbar
 
 new Vue({
